d9e290a023b3ce47639c1ddd95ccde2d96e0ad7f
Tycho Löke <itychohdgames@gmail.com>
2023-08-23 10:48:40 +0200

    Update Windows-Update.ps1

--- Windows-Update.ps1
+++ Windows-Update.ps1
@@ -2,19 +2,24 @@
 # Author: Tycho Loke
 # Distribution: PeopleRock B.V.
 
+# Ensure the directory exists for the transcript
+$transcriptPath = "C:\temp\windowsupdate.log"
+if (!(Test-Path -Path "C:\temp")) {
+    New-Item -ItemType Directory -Force -Path "C:\temp"
+}
+
 # Start transcript to log the output of the script
-Start-Transcript -Path "C:\temp\windowsupdate.log"
+Start-Transcript -Path $transcriptPath
 
 # Asynchronously install the necessary modules and packages
 $installNuGet = Start-Job -ScriptBlock {
     # Check if the NuGet package provider is installed, if not, install it
     if (!(Get-PackageProvider -Name 'Nuget' -ListAvailable)) {
-        Find-PackageProvider -Name 'Nuget' -ForceBootstrap -IncludeDependencies
-        Install-PackageProvider -Name "NuGet" -RequiredVersion "2.8.5.208" -Force
+        Install-PackageProvider -Name "NuGet" -Force -SkipPublisherCheck
     }
     # Check if the PSWindowsUpdate module is installed, if not, install it
     if (!(Get-Module -Name PSWindowsUpdate -ListAvailable)) {
-        Install-Module PSWindowsUpdate -Force
+        Install-Module PSWindowsUpdate -Force -SkipPublisherCheck
     }
 }
 
